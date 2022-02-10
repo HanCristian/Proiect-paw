577869924d1e770e15e284b8aea5e2e3d9b22a53
Han Cristian <tzucu_tzucuyanu@yahoo.com>
2022-02-10 03:09:54 +0200

    Proiect paw

---
+++ valoare/resources/views/layouts/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class ="bg-gray-800 py-10 text-center">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2
+    border-gray-700 ">
+ 
+            <h3 class="">
+                
+            </h3>
+            <h4 class="text-l text-gray-100">
+                <b>PROIECT PAW</b>   
+            <br><br>
+                Han Cristian | Rus Vlad
+            </h4>
+    </div>
+    
+</footer>
